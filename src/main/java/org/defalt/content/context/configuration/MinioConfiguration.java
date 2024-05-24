7a75d33db637234cae9981a3f28a51f72b23595d
adnan <a.aliyari@gmail.com>
2024-05-24 17:36:48 +0330

    added minio host to config files

--- src/main/java/org/defalt/content/context/configuration/MinioConfiguration.java
+++ src/main/java/org/defalt/content/context/configuration/MinioConfiguration.java
@@ -1,15 +1,16 @@
 package org.defalt.content.context.configuration;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfiguration {
     @Bean
-    public MinioClient client() {
+    public MinioClient client(@Value("hosts.minio") String host) {
         return MinioClient.builder()
-                .endpoint("http://osm.minio:9000")
+                .endpoint(host)
                 .credentials("minioadmin", "minioadmin")
                 .build();
     }
