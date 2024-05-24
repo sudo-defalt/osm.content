3551ae8b7036d784c8dba92279bd62dd1b85f0e8
adnan <a.aliyari@gmail.com>
2024-05-24 17:14:10 +0330

    set minio for osm docker cluster

--- src/main/java/org/defalt/content/context/configuration/MinioConfiguration.java
+++ src/main/java/org/defalt/content/context/configuration/MinioConfiguration.java
@@ -1,16 +1,15 @@
 package org.defalt.content.context.configuration;
 
 import io.minio.MinioClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfiguration {
     @Bean
-    public MinioClient client(@Value("${hosts.minio}") String host) {
+    public MinioClient client() {
         return MinioClient.builder()
-                .endpoint(host)
+                .endpoint("http://osm.minio:9000")
                 .credentials("minioadmin", "minioadmin")
                 .build();
     }
